aaa535314ff8e8d72fea79ef6e517d16754a24a5
Ahmad Zaheer Saffi <azs@Ahmads-MacBook-Pro.local>
2019-06-27 18:13:13 -0700

    Adding the case of the OurClass class

---
+++ week4/day7-intro_to_classes/w4h1.py
@@ -0,0 +1,24 @@
+class OurClass():
+
+    def __init__(self, name, location, size=0):
+        self.name = name
+        self.location = location
+        self.size = size
+        self.questions_asked = []
+        if self.size >= 20:
+            self.at_capacity = True
+        else:
+            self.at_capacity = False
+
+   def add_question_asked(self, question):
+        self.questions_asked.append(question)
+
+   def add_class_members(self, num):
+        self.size += num
+
+        if self.size >= 20:
+            print('Capacity Reached!!')
+            self.at_capacity = True
+
+   def check_if_at_capacity(self):
+        return self.at_capacity
